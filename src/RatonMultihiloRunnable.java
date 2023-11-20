5f3dcee1bed243195df5c0e3531890e15d8a47f4
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-11-20 16:00:53 +0100

    RatonMultihilo acabado

--- src/RatonMultihiloRunnable.java
+++ src/RatonMultihiloRunnable.java
@@ -1,8 +1,9 @@
 import java.lang.Thread;
-import java.lang.invoke.SwitchPoint;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
+import static java.lang.Thread.State.*;
+
+
+//Verónica Prieto Prieto
 
 public class RatonMultihiloRunnable implements Runnable {
 
@@ -27,15 +28,13 @@ public class RatonMultihiloRunnable implements Runnable {
     @Override
     public void run (){
         this.comer();
-        //Thread.State Estado = Thread.currentThread().getState();
-        //System.out.println(Thread.currentThread()); //Me dice el nombre del hilo que ha terminado del 0 al 2
-        //System.out.println("state = " + Estado); //Estado del hilo. Solo pone Runnable
+
     }
 
     public static void main (String[] args) throws InterruptedException {
         RatonMultihiloRunnable mickey = new RatonMultihiloRunnable ("Mickey",6);
         RatonMultihiloRunnable jerry = new RatonMultihiloRunnable("Jerry",5);
-        RatonMultihiloRunnable perez = new RatonMultihiloRunnable("Peréz",2);
+        RatonMultihiloRunnable perez = new RatonMultihiloRunnable("Peréz",9);
         long tiempoInicio,tiempoFin;
         System.out.println("-> Inicio de la Ejecución del pragrama Multihilo");
 
@@ -45,40 +44,37 @@ public class RatonMultihiloRunnable implements Runnable {
         Thread hilo_jerry = new Thread(jerry);
         Thread hilo_perez = new Thread(perez);
 
-       System.out.println(hilo_perez.getState());
+        //System.out.println("Pérez = " + hilo_perez.getState()); //aquí esta new
         hilo_mickey.start();
         hilo_jerry.start();
         hilo_perez.start();
 
 
+        ArrayList<Thread.State> Estados = new ArrayList<>(); //Array de estados, variable Enum
+//        Estados.add(NEW);
+//        Estados.add(RUNNABLE);
+//        Estados.add(BLOCKED);
+//        Estados.add(WAITING);
+//        Estados.add(TIMED_WAITING);
+//        Estados.add(TERMINATED);
 
+        // Thread.State estadoAnterior = NEW; //En el bucle siemple va empezar en runnable por lo que el estado anterior siempre es new
 
         while (hilo_perez.isAlive()|| (hilo_jerry.isAlive()) || hilo_perez.isAlive()) {
-            ArrayList <Enum> Estados;
-            //Estados.addElement(hilo_perez.getState());
-            System.out.println();
 
-
-            //Thread.sleep(1000);
+            Thread.State estadoActual = hilo_perez.getState();
+            if (!Estados.contains(estadoActual) ) {
+                Estados.add(estadoActual);
 
             }
-        System.out.println(hilo_perez.getState());
 
-        //try {
-        //   mickey.join();
-        //   jerry.join();
-        //   perez.join();
-        //}catch (InterruptedException e){
-        //   e.printStackTrace();
-        //}
+        }
+        System.out.println("");
+        System.out.println("El ratón Pérez a pasado por los estados: " + Estados);
+        //System.out.println("Pérez = " + hilo_perez.getState()); //Terminated siempre va a estar fuera del bucle, no se imprimirá aunque este como variable
+
 
         tiempoFin = System.currentTimeMillis();
-
-        //if(mickey.tiempoAlimentacion < jerry.tiempoAlimentacion){
-        //    tiempoFin = jerry.tiempoAlimentacion*1000;
-        //} else if (jerry.tiempoAlimentacion < perez.tiempoAlimentacion) {
-        //    tiempoFin = perez.tiempoAlimentacion*1000;
-        //}else tiempoFin = mickey.tiempoAlimentacion*1000;
 
 
         System.out.println("-> Fin de la Ejecución. Tiempo transcurrido: " +(int)(((tiempoFin-tiempoInicio)/1000)) + " segundos"); // (int)(((tiempoFin-tiempoInicio)/1e9)) 1 elevado a 9 para convertir los nanos segundos a segundos
---
+++ src/alumnos/RatonMultihiloRunnableEntrega.java
@@ -0,0 +1,86 @@
+package alumnos;
+//Verónica Prieto Prieto
+
+
+import java.lang.Thread;
+import java.util.ArrayList;
+
+
+
+public class RatonMultihiloRunnableEntrega implements Runnable {
+
+    private String nombre;
+    private long tiempoAlimentacion;
+
+    public RatonMultihiloRunnableEntrega(String nombre, int tiempoAlimentacion){
+        this.nombre = nombre;
+        this.tiempoAlimentacion = tiempoAlimentacion;
+    }
+
+    public void comer(){
+        try{
+            System.out.println(" | -> El ratón " + nombre + " ha comenzado a alimentarse.");
+            Thread.sleep(tiempoAlimentacion*1000);
+            System.out.println( " | <- El ratón " + nombre + " ha terminado de alimentarse");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run (){
+        this.comer();
+    }
+
+    public static void main (String[] args) throws InterruptedException {
+        RatonMultihiloRunnableEntrega mickey = new RatonMultihiloRunnableEntrega("Mickey", 1);
+        RatonMultihiloRunnableEntrega jerry = new RatonMultihiloRunnableEntrega("Jerry", 1);
+        RatonMultihiloRunnableEntrega perez = new RatonMultihiloRunnableEntrega("Peréz", 1);
+        RatonMultihiloRunnableEntrega mickey1 = new RatonMultihiloRunnableEntrega("Mickey1", 1);
+        RatonMultihiloRunnableEntrega jerry1 = new RatonMultihiloRunnableEntrega("Jerry1", 1);
+        RatonMultihiloRunnableEntrega perez1 = new RatonMultihiloRunnableEntrega("Peréz1", 1);
+        RatonMultihiloRunnableEntrega mickey2 = new RatonMultihiloRunnableEntrega("Mickey2", 1);
+        RatonMultihiloRunnableEntrega jerry2= new RatonMultihiloRunnableEntrega("Jerry2", 1);
+        RatonMultihiloRunnableEntrega perez2 = new RatonMultihiloRunnableEntrega("Peréz2", 1);
+        long tiempoInicio, tiempoFin;
+        System.out.println("-> Inicio de la Ejecución del pragrama Multihilo");
+
+        tiempoInicio = System.currentTimeMillis();
+
+        Thread hilo_mickey = new Thread(mickey);
+        Thread hilo_jerry = new Thread(jerry);
+        Thread hilo_perez = new Thread(perez);
+        Thread hilo_mickey1 = new Thread(mickey1);
+        Thread hilo_jerry1 = new Thread(jerry1);
+        Thread hilo_perez1 = new Thread(perez1);
+        Thread hilo_mickey2 = new Thread(mickey2);
+        Thread hilo_jerry2 = new Thread(jerry2);
+        Thread hilo_perez2 = new Thread(perez2);
+
+        hilo_mickey.start();
+        hilo_jerry.start();
+        hilo_perez.start();
+        hilo_mickey1.start();
+        hilo_jerry1.start();
+        hilo_perez1.start();
+        hilo_mickey2.start();
+        hilo_jerry2.start();
+        hilo_perez2.start();
+
+        ArrayList<Thread.State> Estados = new ArrayList<>();
+
+        while (hilo_mickey.isAlive() || (hilo_jerry.isAlive()) || hilo_perez.isAlive() || hilo_perez1.isAlive() || (hilo_jerry1.isAlive()) || hilo_mickey1.isAlive() || hilo_perez2.isAlive() || (hilo_jerry2.isAlive()) || hilo_mickey2.isAlive()) {
+
+            Thread.State estadoActual = hilo_perez.getState();
+            if (!Estados.contains(estadoActual)) {
+                Estados.add(estadoActual);
+            }
+
+        }
+        System.out.println("");
+        System.out.println("El ratón Pérez ha pasado por los estados: " + Estados);
+
+        tiempoFin = System.currentTimeMillis();
+
+        System.out.println("-> Fin de la Ejecución. Tiempo transcurrido: " + (int) (((tiempoFin - tiempoInicio) / 1000)) + " segundos");
+    }
+}
