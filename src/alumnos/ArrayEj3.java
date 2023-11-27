769f7e9b5c9712fa156ea6af03cdf87073678098
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-11-27 16:00:17 +0100

    27.11.2023 16.00

--- src/alumnos/ArrayEj3.java
+++ src/alumnos/ArrayEj3.java
@@ -1,33 +1,47 @@
 package alumnos;
 
-//Veronica Prieto Prieto
-
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /*
-Realiza un programa que calcule la suma de un array de números enteros. El tamaño del array se solicitará al usuario por pantalla, y
-posteriormente se asignará, de forma aleatoria un valor entero (entre 0 y 100) para cada uno de los elementos del array.
+Realiza un programa que calcule la suma de un array de números enteros.
+El tamaño del array se solicitará al usuario por pantalla, y
+posteriormente se asignará, de forma aleatoria un valor entero
+(entre 0 y 100) para cada uno de los elementos del array.
 
 Realiza la suma de los elementos del array:
-Mediante un método secuencial, tradicional
-Mediante un método multihilo, dividiendo el array en dos trozos iguales, realizando la suma parcial de cada trozo y posteriormente
-sumando ambas sumas parciales.
-Comprueba que ambos resultados son iguales y calcula el tiempo que se tarda en realizar el cálculo mediante ambos métodos.
+1. Mediante un método secuencial, tradicional
 
-Nota: Para que los resultados sean más evidentes, puedes “retrasar artificialmente” la ejecución de la suma de cada elemento usando
+2. Mediante un método multihilo, dividiendo el array en dos trozos iguales,
+realizando la suma parcial de cada trozo y posteriormente
+sumando ambas sumas parciales.
+3. Comprueba que ambos resultados son iguales y calcula el tiempo
+que se tarda en realizar el cálculo mediante ambos métodos.
+
+Nota: Para que los resultados sean más evidentes, puedes “retrasar artificialmente”
+la ejecución de la suma de cada elemento usando
 Thread.sleep (<numero_milisegundos>). Con 1 milisegundo suele ser suficiente.
  */
-
-
 public class ArrayEj3 {
-
     static Scanner sc = new Scanner(System.in);
-    ArrayList<Integer> calcular = new ArrayList<Integer>();
+    static Random ran = new Random();
+    public static void main(String[] args){
 
-     //System.out.println("")
+        System.out.println("Introduce la cantida de números que quieras sumar. Los números se crearan aleatoriamente");
+        int cantidad = sc.nextInt();
 
+        ArrayList<Integer> numerosSuma = new ArrayList<>();
 
-
+        for (int i = 0; i < cantidad; i++){
+            int numeroRandom = ran.nextInt(100);
+            numerosSuma.add(numeroRandom);
+        }
+        System.out.println(numerosSuma);
+        int resultado = 0;
+        for (int i = 0; i < numerosSuma.size(); i++){
+            resultado += numerosSuma.get(i);
+        }
+        System.out.println(resultado);
+    }
 }
